86f8f35dcf9f93f52a7e15370e5acb7aa1432488
Karen <kjgamis@live.ca>
2018-04-11 17:42:51 -0400

    editing pictures

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
-  post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
+  post 'pictures' => 'pictures#create'
 
   get 'pictures/:id' => 'pictures#show'
+
+  get 'pictures:id/edit' => 'pictures#edit'
+  patch 'pictures/:id' => 'pictures#update'
+
 end
