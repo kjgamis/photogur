04eb56347dec0195b88047152dcc38f54c92c1d4
Karen <kjgamis@live.ca>
2018-04-20 14:34:21 -0400

    add users model, controller, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'pictures#index'
+  # root 'pictures#index'
+  root 'users#new'
 
   resources :pictures
+
+  resource :user, only: [:new, :create]
 
 end
