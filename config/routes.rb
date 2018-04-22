094ce1ca0dd539d0de0759786397e034d921e92f
Karen <kjgamis@live.ca>
2018-04-22 19:42:34 -0400

    add home page with sign up, sign in and logout links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # root 'pictures#index'
-  root 'users#new'
+  root 'home#index'
 
   resources :pictures
 
